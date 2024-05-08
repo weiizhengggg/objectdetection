b327624de17656e3246b279285ddec6955ea83f6
weiizhengggg <124341838+weiizhengggg@users.noreply.github.com>
2024-05-08 14:50:53 +0800

    Update faster-rcnn_r101_afpn_1x_nwpuvhr10.py

--- mmdetection/configs/faster_rcnn-fyp/faster-rcnn_r101_afpn_1x_nwpuvhr10.py
+++ mmdetection/configs/faster_rcnn-fyp/faster-rcnn_r101_afpn_1x_nwpuvhr10.py
@@ -12,7 +12,7 @@ model = dict(
         pad_size_divisor=32),
 
     backbone=dict(
-        type='ResNet',
+        type='ResNet_CBAM',
         depth=101,
         num_stages=4,
         out_indices=(0, 1, 2, 3),
