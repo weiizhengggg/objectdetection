e62e1c8b3b8518968b3ec87b0846a08b0702376a
weiizhengggg <kikuchitouma00@gmail.com>
2024-05-18 16:23:34 +0800

    Use ResNet-50

--- mmdetection/configs/faster_rcnn-fyp/faster-rcnn_r101_afpn_1x_nwpuvhr10.py
+++ mmdetection/configs/faster_rcnn-fyp/faster-rcnn_r101_afpn_1x_nwpuvhr10.py
@@ -13,14 +13,14 @@ model = dict(
 
     backbone=dict(
         type='ResNet_CBAM',
-        depth=52,
+        depth=50,
         num_stages=4,
         out_indices=(0, 1, 2, 3),
         frozen_stages=1,
         norm_cfg=dict(type='BN', requires_grad=True),
         norm_eval=True,
         style='pytorch',
-        init_cfg=dict(type='Pretrained', checkpoint='torchvision://resnet101')),
+        init_cfg=dict(type='Pretrained', checkpoint='torchvision://resnet50')),
 
         # neck=dict(
         #     type='AFPN',
