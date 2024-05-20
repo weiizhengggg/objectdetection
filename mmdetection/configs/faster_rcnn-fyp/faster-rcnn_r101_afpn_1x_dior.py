d89cc977ec794472f5db777a480471d92045eca6
weiizhengggg <kikuchitouma00@gmail.com>
2024-05-21 00:36:26 +0800

    try pafpn

--- mmdetection/configs/faster_rcnn-fyp/faster-rcnn_r101_afpn_1x_dior.py
+++ mmdetection/configs/faster_rcnn-fyp/faster-rcnn_r101_afpn_1x_dior.py
@@ -20,10 +20,16 @@ model = dict(
         style='pytorch',
         init_cfg=dict(type='Pretrained', checkpoint='torchvision://resnet101')),
 
+    # neck=dict(
+    #     type='AFPN',
+    #     in_channels=[256, 512, 1024, 2048],
+    #     out_channels=256),
+
     neck=dict(
-        type='AFPN',
+        type='PAFPN',
         in_channels=[256, 512, 1024, 2048],
-        out_channels=256),
+        out_channels=256,
+        num_outs=5),
 
     rpn_head=dict(
         type='RPNHead',
