ce619a0d76ece8d923691a40d0282b2de668c87c
weiizhengggg <kikuchitouma00@gmail.com>
2024-05-26 19:15:11 +0800

    update number of workers

--- mmdetection/configs/faster_rcnn-fyp/faster-rcnn_r101_pafpn_1x_nwpuvhr10.py
+++ mmdetection/configs/faster_rcnn-fyp/faster-rcnn_r101_pafpn_1x_nwpuvhr10.py
@@ -150,8 +150,8 @@ test_pipeline = [
 ]
 
 train_dataloader = dict(
-    batch_size=4,
-    num_workers=8,
+    batch_size=2,
+    num_workers=4,
     persistent_workers=True,
     sampler=dict(type='DefaultSampler', shuffle=True),
     batch_sampler=dict(type='AspectRatioBatchSampler'),
