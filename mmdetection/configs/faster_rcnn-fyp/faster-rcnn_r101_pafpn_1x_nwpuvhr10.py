79b4ffb11bfcbcea61211d79a95c272d0e1deef5
weiizhengggg <kikuchitouma00@gmail.com>
2024-05-29 22:29:27 +0800

    remove dcn

--- mmdetection/configs/faster_rcnn-fyp/faster-rcnn_r101_pafpn_1x_nwpuvhr10.py
+++ mmdetection/configs/faster_rcnn-fyp/faster-rcnn_r101_pafpn_1x_nwpuvhr10.py
@@ -20,9 +20,7 @@ model = dict(
         norm_cfg=dict(type='BN', requires_grad=True),
         norm_eval=True,
         style='pytorch',
-        init_cfg=dict(type='Pretrained', checkpoint='torchvision://resnet101'),
-        dcn=dict(type='DCNv2', deform_groups=1, fallback_on_stride=False),
-        stage_with_dcn=(False, True, True, True)),
+        init_cfg=dict(type='Pretrained', checkpoint='torchvision://resnet101')),
     
     neck=dict(
         type='PAFPN',
